db4f6adbb6e341cc96e2e252a5ef298e4c6fb4b5
Михаил Козин <Vasago85@mail.ru>
2022-05-10 20:03:12 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Console;
+
+namespace lesson___2_7_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)  // точка входа
+        {
+            string fName = "Михаил";
+            string lName = "Козин";
+            string Email = "Vasago85@mail.ru";
+            string Sprogramming = "0";
+            string Smathematics = "3";
+            string Sphysics = "3";
+
+
+
+            WriteLine($"Имя: {fName} \nФамилия: {lName} \nпочта: {Email} \nбаллы по программированию: {Sprogramming} \nбаллы по математике: {Smathematics} \nбаллы по физике: {Sphysics}");
+
+            ReadLine();  // задержка закрывается Enter
+        }
+    }
+}
